29fb05d61df9c2af16dbe37a1c8515d9731206ad
jeugfer600 <jeugfer600@g.educaand.es>
2024-02-23 08:33:22 +0100

    initial commit

---
+++ src/main/java/com/example/practicaramaspostgresql/PracticaRamasPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaramaspostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaRamasPostgreSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaRamasPostgreSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicaramaspostgresql/PracticaRamasPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaramaspostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaRamasPostgreSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
