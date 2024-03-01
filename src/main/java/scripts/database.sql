262f564339fd5a5028a760c322dce99f770cf062
= <=>
2024-03-01 08:49:18 +0100

    commit sql terminado

--- src/main/java/scripts/database.sql
+++ src/main/java/scripts/database.sql
@@ -16,3 +16,24 @@ CREATE TABLE partida(
                         Foreign Key (id_juego)
                             references juego (idJuego)
 );
+
+INSERT INTO juego (titulo, descripcion, plataforma, favorito, JugarMasTarde) VALUES ('EA Sport 24', 'Juego de simulacion de futbol', 'PS5', true, true);
+
+INSERT INTO juego (titulo, descripcion, plataforma, favorito, JugarMasTarde) VALUES ('PES24', 'Juego de simulacion de futbol', 'PS4' , false, false);
+
+INSERT INTO juego (titulo, descripcion, plataforma, favorito, JugarMasTarde) VALUES ('CALL OF DUTY', 'Juego guerra', 'PS5', false, true);
+
+INSERT INTO juego (titulo, descripcion, plataforma, favorito, JugarMasTarde) VALUES ('GTA5', ' Accion', 'PC' , true, false);
+
+INSERT INTO juego (titulo, descripcion, plataforma, favorito, JugarMasTarde) VALUES ('F1 2024', 'Carrera', 'PC' , true, false);
+
+
+INSERT INTO partida (review, puntuacion, tiempoJuego, id_juego) VALUES ('review1', 150, 50, 1);
+
+INSERT INTO partida (review, puntuacion, tiempoJuego, id_juego) VALUES ('review2', 25 ,  75, 2);
+
+INSERT INTO partida (review, puntuacion, tiempoJuego, id_juego) VALUES ('review3', 800, 125, 3);
+
+INSERT INTO partida (review, puntuacion, tiempoJuego, id_juego) VALUES ('review4', 500, 200, 4);
+
+INSERT INTO partida (review, puntuacion, tiempoJuego, id_juego) VALUES ('review5', 410, 100, 5);
